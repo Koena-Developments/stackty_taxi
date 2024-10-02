d82674550470cc377d91027c74b5bb622e794699
tmotswenyane023b <tmotswenyane023b@student.wethinkcode.co.za>
2024-10-02 11:47:36 +0200

    cleaned some errors in the data base and added admin tables

--- TAXI_APP/stacty_trans/admin.py
+++ TAXI_APP/stacty_trans/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
-
+from .models import User, Fare_ride, Taxi_Driver
 # Register your models here.
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email')  
+    search_fields = ('name', 'email') 
+
+admin.site.register(User, UserAdmin)
+
+
+class Taxi_DriverAdmin(admin.ModelAdmin):
+    list_display = ('name', 'license_number') 
+    search_fields = ('name', 'destination') 
+admin.site.register(Taxi_Driver, Taxi_DriverAdmin)
+
+class FareRideAdmin(admin.ModelAdmin):
+    list_display = ('user', 'request_time', 'status')  
+    list_filter = ('status',) 
+    search_fields = ('user__name', 'status')
+
+
+admin.site.register(Fare_ride, FareRideAdmin)
