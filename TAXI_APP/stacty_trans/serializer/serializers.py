77475c66b191a407379174b919b06f8630e5bdb3
tmotswenyane023b <tmotswenyane023b@student.wethinkcode.co.za>
2024-10-02 15:50:39 +0200

    unstuck

--- TAXI_APP/stacty_trans/serializer/serializers.py
+++ TAXI_APP/stacty_trans/serializer/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from models import User, Taxi_Driver, Fare_ride
+from ..models import User, Taxi_Driver, Fare_ride
 
 
 class UserSerializer(serializers.ModelSerializer):
--- TAXI_APP/stacty_trans/urls.py
+++ TAXI_APP/stacty_trans/urls.py
@@ -6,7 +6,7 @@ from .VIEWS.auth_views import LoginView, LogoutView
 urlpatterns = [
     path('api/login/', LoginView.as_view(), name='login'),
     path('api/logout/', LogoutView.as_view(), name='logout'),
-    path('home', TaxiDriverViewSet, name='home'),
+    path('home', TaxiDriverViewSet.as_view, name='home'),
      
 
 ]
